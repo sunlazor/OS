52d8b44a88528504adbc1db878e4acef5529ad96
sunlazor <qubicus@gmail.com>
2013-12-29 22:30:55 +0300

    Update lab3.client.c

--- lab3.client.c
+++ lab3.client.c
@@ -2,13 +2,20 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
+#include <sys/stat.h>
 #include <string.h>
 #include <errno.h>
 
-struct msgbuf {
-  long type;
+struct client {
+  long status;
   char text[555];
-} buff;
+} client;
+
+enum statuses
+{
+  IDLE, C_CONN, C_DISC, C_TIME, 
+  S_CONN, S_DISC, S_TIME, COND, EXIT
+};
 
 int main()
 {
@@ -17,23 +24,126 @@ int main()
   char key_num = 111;
   key_t key;
   int key_id;
-  struct msgbuf buff;
+  // char status = 1;
+  client.status = IDLE;
   //char text[] = "YO BRO, YOUR SERVER SUX";
 
-  buff.type = 1;
+  // buff.type = 1;
   //memcpy(buff.text, text, sizeof(text));
-  strcpy(buff.text, "YO BRO, YOUR SERVER SUX");
-  printf("text:%s\n", buff.text);
+  // strcpy(client.text, "YO BRO, YOUR SERVER SUX");
+  // printf("text:%s\n", buff.text);
 
   key = ftok(path, key_num);
-  key_id = msgget(key, IPC_CREAT | 0002 | 0004 | 0200 | 0400);
-  printf("key_id:%d\n", key_id);
+  key_id = msgget(key, IPC_CREAT | S_IRUSR | S_IWUSR);
+  // printf("key_id:%d\n", key_id);
 
-  printf("msgsnd status:%d\n", msgsnd(key_id, (void *)&buff, sizeof(buff.text), 0));
-  printf("errno #%d: %s\n", errno, strerror(errno));
+  // printf("msgsnd status:%d\n", msgsnd(key_id, &buff, sizeof(buff.text), 0));
+  // printf("errno #%d: %s\n", errno, strerror(errno));
 
   //printf("msgctl status:%d\n", msgctl(key_id, IPC_RMID, NULL));
 
-  printf("Sent!\n");
+  while(client.status != EXIT)
+  {
+    switch(client.status)
+    {
+      case IDLE:
+      {
+        printf("Insert command\n8: Client exit\n1: Connect server\n");
+        scanf("%d", &client.status);
+        // printf("Client status: %d\n", client.status);
+      } break;
+      case C_CONN:
+      {
+        printf("C_CONN\n");
+        int r_msgsnd = msgsnd(key_id, &client, 0, IPC_NOWAIT);
+        // printf("msgsnd status: %d\n", r_msgsnd);
+        // printf("errno #%d: %s\n", errno, strerror(errno));
+        if(r_msgsnd == -1)
+        {
+          printf("msgsnd status: %d\n", r_msgsnd);
+          printf("errno #%d: %s\n", errno, strerror(errno));
+          client.status = EXIT;
+          break;
+        } 
+        else
+        {
+          printf("msgsnd OK\n");
+          int r_msgrcv = msgrcv(key_id, &client, 0, S_CONN, 0);
+          if(r_msgrcv == -1)
+          {
+            printf("msgrcv status: %d\n", r_msgrcv);
+            printf("errno #%d: %s\n", errno, strerror(errno));
+            client.status = EXIT;
+            break;
+          }
+          else printf("msgrcv OK\n");          
+        }
+        printf("Connected!\n");
+        printf("Insert command\n2: Disconnect server\n3: Ask message\n");
+        scanf("%d", &client.status);        
+      } break;
+      case COND:
+      {
+        printf("Insert command\n2: Disconnect server\n3: Ask message\n");
+        scanf("%d", &client.status);
+      } break;
+      case C_DISC:
+      {
+        int r_msgsnd = msgsnd(key_id, &client, 0, IPC_NOWAIT);
+        if(r_msgsnd == -1)
+        {
+          printf("msgsnd status: %d\n", r_msgsnd);
+          printf("errno #%d: %s\n", errno, strerror(errno));
+          client.status = EXIT;
+          break;
+        } 
+        else
+        {
+          printf("msgsnd OK\n");
+          int r_msgrcv = msgrcv(key_id, &client, 0, S_DISC, 0);
+          if(r_msgrcv == -1)
+          {
+            printf("msgrcv status: %d\n", r_msgrcv);
+            printf("errno #%d: %s\n", errno, strerror(errno));
+            client.status = EXIT;
+            break;
+          }
+          else printf("msgrcv OK\n");        
+        }
+        printf("Disonnected!\n");
+        client.status = IDLE;
+      } break;
+      case C_TIME:
+      {
+        int r_msgsnd = msgsnd(key_id, &client, 0, IPC_NOWAIT);
+        if(r_msgsnd == -1)
+        {
+          printf("msgsnd status: %d\n", r_msgsnd);
+          printf("errno #%d: %s\n", errno, strerror(errno));
+          client.status = EXIT;
+          break;
+        } 
+        else
+        {
+          printf("msgsnd OK\n");
+          int r_msgrcv = msgrcv(key_id, &client, sizeof(client.text[555]), S_TIME, 0);
+          if(r_msgrcv == -1)
+          {
+            printf("msgrcv status: %d\n", r_msgrcv);
+            printf("errno #%d: %s\n", errno, strerror(errno));
+            client.status = EXIT;
+            break;
+          }
+          else printf("msgrcv OK\n");        
+        }
+        printf("Message: %s\n", client.text);
+        client.status = COND;
+      } break;
+    }
+    sleep(1);
+    printf("Next iteration\n");
+  }
+
+  printf("Client exit\n");
   return 0;
 }
