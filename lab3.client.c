f730844b561479c5ebf7e4ae71a0c649cd0791a6
sunlazor <qubicus@gmail.com>
2014-01-09 16:12:05 +0300

    Update lab3.client.c

--- lab3.client.c
+++ lab3.client.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
+#include <sys/ipc.h> 
+#include <sys/msg.h> 
 #include <sys/stat.h>
 #include <string.h>
 #include <errno.h>
 
-struct client {
-  long status;
+struct client {  
+  long status;   
   char text[555];
 } client;
 
-enum statuses
+enum
 {
   IDLE, C_CONN, C_DISC, C_TIME, 
   S_CONN, S_DISC, S_TIME, COND, EXIT
@@ -22,10 +22,11 @@ int main()
   //char path[] = "~/os/lab2/data";
   char path[] = "data";
   char key_num = 111;
-  key_t key;
+  key_t key; 
   int key_id;
-  // char status = 1;
+  // char status = 1;  
   client.status = IDLE;
+  char stats = IDLE;
   //char text[] = "YO BRO, YOUR SERVER SUX";
 
   // buff.type = 1;
@@ -42,19 +43,27 @@ int main()
 
   //printf("msgctl status:%d\n", msgctl(key_id, IPC_RMID, NULL));
 
-  while(client.status != EXIT)
+  while(stats != EXIT)
   {
-    switch(client.status)
+    printf("stats stats: %d\n", stats);
+    switch(stats)
     {
       case IDLE:
       {
-        printf("Insert command\n8: Client exit\n1: Connect server\n");
-        scanf("%d", &client.status);
-        // printf("Client status: %d\n", client.status);
+        printf("Insert command\n1: Connect server\n8: Exit client\n");
+        int r_scanf = scanf("%d", &stats);
+        printf("scanf status: %d\n", r_scanf);
+        printf("stats stats: %d\n", stats); 
+        if((stats < IDLE) || (stats > EXIT))
+        {
+          printf("Invalid command!\n");
+          stats = IDLE;
+        }
       } break;
       case C_CONN:
       {
-        printf("C_CONN\n");
+        //printf("C_CONN\n");  
+        client.status = C_CONN;
         int r_msgsnd = msgsnd(key_id, &client, 0, IPC_NOWAIT);
         // printf("msgsnd status: %d\n", r_msgsnd);
         // printf("errno #%d: %s\n", errno, strerror(errno));
@@ -62,9 +71,9 @@ int main()
         {
           printf("msgsnd status: %d\n", r_msgsnd);
           printf("errno #%d: %s\n", errno, strerror(errno));
-          client.status = EXIT;
+          stats = EXIT;
           break;
-        } 
+        }   
         else
         {
           printf("msgsnd OK\n");
@@ -73,85 +82,97 @@ int main()
           {
             printf("msgrcv status: %d\n", r_msgrcv);
             printf("errno #%d: %s\n", errno, strerror(errno));
-            client.status = EXIT;
+            stats = EXIT;
             break;
           }
-          else printf("msgrcv OK\n");          
-        }
-        printf("Connected!\n");
-        printf("Insert command\n2: Disconnect server\n3: Ask message\n");
-        scanf("%d", &client.status);        
+          else 
+          {
+            printf("msgrcv OK\n");
+            stats = COND;
+          }
+        }  
       } break;
       case COND:
       {
+        printf("Stats %d\n", stats);
         printf("Insert command\n2: Disconnect server\n3: Ask message\n");
-        scanf("%d", &client.status);
+        int r_scanf = scanf("%d", &stats);
+        printf("scanf status: %d\n", r_scanf);
+        printf("stats stats: %d\n", stats);   
+        if((stats < IDLE) || (stats > EXIT))  
+        {
+          printf("Invalid command!\n");
+          stats = COND;
+        }
       } break;
       case C_DISC:
       {
+        client.status = C_DISC;
         int r_msgsnd = msgsnd(key_id, &client, 0, IPC_NOWAIT);
         if(r_msgsnd == -1)
         {
           printf("msgsnd status: %d\n", r_msgsnd);
           printf("errno #%d: %s\n", errno, strerror(errno));
-          client.status = EXIT;
+          stats = EXIT;
           break;
         } 
         else
-        {
+        {   
           printf("msgsnd OK\n");
           int r_msgrcv = msgrcv(key_id, &client, 0, S_DISC, 0);
           if(r_msgrcv == -1)
           {
             printf("msgrcv status: %d\n", r_msgrcv);
             printf("errno #%d: %s\n", errno, strerror(errno));
-            client.status = EXIT;
+            stats = EXIT;
             break;
           }
-          else printf("msgrcv OK\n");        
+          else printf("msgrcv OK\n");
         }
         printf("Disonnected!\n");
-        client.status = IDLE;
+        stats = IDLE;
       } break;
       case C_TIME:
       {
+        client.status = C_TIME;
         int r_msgsnd = msgsnd(key_id, &client, 0, IPC_NOWAIT);
         if(r_msgsnd == -1)
         {
           printf("msgsnd status: %d\n", r_msgsnd);
           printf("errno #%d: %s\n", errno, strerror(errno));
-          client.status = EXIT;
+          stats = EXIT;
           break;
         } 
         else
-        {
+        {   
           printf("msgsnd OK\n");
           int r_msgrcv = msgrcv(key_id, &client, sizeof(client.text), S_TIME, 0);
           if(r_msgrcv == -1)
           {
             printf("msgrcv status: %d\n", r_msgrcv);
             printf("errno #%d: %s\n", errno, strerror(errno));
-            client.status = EXIT;
+            stats = EXIT;
             break;
           }
-          else printf("msgrcv OK\n");        
+          else printf("msgrcv OK\n");
         }
         printf("Message: %s\n", client.text);
-        client.status = COND;
+        stats = COND;
       } break;
-      default: 
+      default:
       {
-        printf("Unknown command!");
+        printf("Unknown command!\n");
         client.status = C_DISC;
         msgsnd(key_id, &client, 0, IPC_NOWAIT);
-        msgrcv(key_id, &client, 0, S_DISC, 0);
-        client.status = EXIT;
+        msgrcv(key_id, &client, 0, S_DISC, 0); 
+        stats = EXIT;
       }
-    }
+    }  
     sleep(1);
-    printf("Next iteration\n");
+    printf("Stats status: %d\n", stats);
+    //printf("Next iteration\n");
   }
-
+   
   printf("Client exit\n");
   return 0;
 }
